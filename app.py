f5bbd6a2bb2ba023ff85e0245caeba3893d2e235
Lincoln <lincolnruwambara@gmail.com>
2023-11-05 17:17:09 +0100

    Adding more functionality to the application

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from questions import questions
+from Questions import Questions
    
+
+quest=[
+            "What is the primary goal of data preprocessing in data science?\na) Data visualization\nb) Data cleaning and transformation\nc) Model training\nd) Feature engineering\n\n",
+            "Which of the following is NOT a supervised learning algorithm?\na) Linear Regression\nb) Decision Tree\nc) K-Means Clustering\nd) Support Vector Machine\n\n" , 
+            "In a classification problem, what is the target variable or output typically referred to as?\na) Feature\nb) Predictor\nc) Label\nd) Input\n\n"
+            ]
+
+
+
+
 
 questions=[
-            "What is the primary goal of data preprocessing in data science?\na) Data visualization\nb) Data cleaning and transformation\nc) Model training\nd) Feature engineering\n\n",
-            "Which of the following is NOT a supervised learning algorithm?/na) Linear Regression\nb) Decision Tree\nc) K-Means Clustering\nd) Support Vector Machine\n\n"  
-            "In a classification problem, what is the target variable or output typically referred to as?\na) Feature\nb) Predictor\nc) Label\nd) Input"
-            ]
-        
-   
-for question in questions:
-        print(question)
+        Questions(quest[0],"a"),
+        Questions(quest[1],"b"),
+        Questions(quest[2],"c")
+]
 
+
+
+def run(questions):
+    score=0
+    for i in questions:
+        answer = input(i.prompt)
+        if answer == i.answer:
+            score +=1
+    print(score)        
+
+
+run(questions)
