574af628e0a4e07b875bb3b7a61326c628efbf19
Lincoln <lincolnruwambara@gmail.com>
2023-11-05 17:16:38 +0100

    Added an array that will store the question and answer as an object

--- questions.py
+++ questions.py
@@ -1,18 +1,13 @@
-class questions:
+class Questions:
 
     def __init__(self,prompt,answer):
 
         self.prompt = prompt
         self.answer = answer
         
-        questions=[
-            "What is the primary goal of data preprocessing in data science?\na) Data visualization\nb) Data cleaning and transformation\nc) Model training\nd) Feature engineering\n\n",
-            "Which of the following is NOT a supervised learning algorithm?/na) Linear Regression\nb) Decision Tree\nc) K-Means Clustering\nd) Support Vector Machine\n\n"  
-            "In a classification problem, what is the target variable or output typically referred to as?\na) Feature\nb) Predictor\nc) Label\nd) Input"
-            ]
+        
         
    
-        for question in questions:
-            print(question)
+        
 
 
